f131a406e392aafbed20965368ea107aae9fde95
Ian Miller <theiandmiller@gmail.com>
2022-02-08 18:41:03 -0600

    REACT Toggle Display with &&

--- toggleDisplay.js
+++ toggleDisplay.js
@@ -12,19 +12,11 @@ class MyComponent extends React.Component {
     }));
   }
   render() {
-    if (this.state.display) {
-      return (
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-          <h1>Displayed!</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+      </div>
+    );
   }
 }
